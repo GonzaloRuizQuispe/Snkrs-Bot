2bdd10c588284792977794791c48f591caea692a
ChachaJr <62969610+GonzaloRuizQuispe@users.noreply.github.com>
2022-03-12 12:24:26 -0300

    Bot Developers Modificate

--- Terminals/Bots/Developers-Index.py
+++ Terminals/Bots/Developers-Index.py
@@ -21,10 +21,15 @@ async def on_member_join(ctx):
     autorole = discord.utils.get(ctx.guild.roles, name = '𝓝𝓔𝓦')
     await ctx.add_roles(autorole)
 
+@client.command(pass_context = True) 
+async def clear(ctx, num_parametro = 10):
+    await ctx.channel.purge(limit = int(num_parametro))
+    await ctx.send("Terminado")
+
 @client.command(pass_context=True)
 async def Help(ctx):
     await ctx.message.delete()
-    await ctx.send('```*Mis Comandos Son*\nd/producto name_web <link_producto>\n```')
+    await ctx.send(':mobile_phone:**Mis Comandos Son**:mobile_phone:\n```d/producto name_web <link_producto>\nd/seccion name_web <link_seccion>\nname_web : grid/etc```')
 
 @client.command(pass_context=True)
 @commands.has_role('𝓐𝓓𝓜𝓘𝓝𝓘𝓢𝓣𝓡𝓐𝓣𝓞𝓡')
@@ -50,14 +55,14 @@ async def producto(ctx,web,url):
             await ctx.message.delete()
             
             msg = await ctx.send("Este link fue agregado correctamente")
-            time(10)
+            time.sleep(5)
             await msg.delete()
 
-        if cantidad_f[0] == url[1:-1] :
+        elif cantidad_f[0] == url[1:-1] :
             await ctx.message.delete()
 
             msg = await ctx.send("Ya Existe En La DB")
-            time(10)
+            time.sleep(5)
             await msg.delete()
 
 @client.command(pass_context=True)
@@ -75,23 +80,23 @@ async def seccion(ctx,web,url):
     if connection.is_connected():
         cursor = connection.cursor()
 
-        cursor.execute("SELECT URL_WEB FROM secciones_"+web+" WHERE URL_WEB='"+url[1:-1]+"'")
+        cursor.execute("SELECT URL_Seccion FROM secciones_"+web+" WHERE URL_Seccion='"+url[1:-1]+"'")
         cantidad_f = cursor.fetchone()
 
         if cantidad_f == None:
-            cursor.execute("INSERT INTO secciones_"+web+" (URL_WEB) VALUES ('"+url[1:-1]+"')")
+            cursor.execute("INSERT INTO secciones_"+web+" (URL_Seccion) VALUES ('"+url[1:-1]+"')")
             connection.commit()
             await ctx.message.delete()
 
             msg = await ctx.send("Esta sección se agrego correctamente")
-            time(10)
+            time.sleep(5)
             await msg.delete()
 
-        if cantidad_f[0] == url[1:-1] :
+        elif cantidad_f[0] == url[1:-1] :
             await ctx.message.delete()
 
             msg = await ctx.send("Esta seccion ya existe en la DB")
-            time(10)
+            time.sleep(5)
             await msg.delete()
 
 
